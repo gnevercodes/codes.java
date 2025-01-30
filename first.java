574e749699a0521892c248238b8334e3d4606b9a
ganesh <gnevercodes@gmail.com>
2025-01-29 19:20:46 -0600

    fasdl;fads;lfjadsl

--- first.java
+++ first.java
@@ -1,6 +1,25 @@
-class animals
-{
+class animals{
     public static void main(String[] args) {
-        System.out.println("hello");
+        final dog d// object reference vari // 
+         = new dog()   ; 
+        d.size = 43; 
+        d.age = 3 ; 
+        d.name = "tommy"; 
+        d.bark() ; 
+    }
+}
+
+class dog { 
+    int size ; 
+    int age ; 
+    String name ; 
+    double d = 34.4; 
+    boolean bark = true ; 
+
+    void bark(){
+        System.out.println("woof woof");
+    } 
+    public void play(){
+        System.out.println("playing"); 
     }
 }
